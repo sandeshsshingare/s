8acb633aa877b33101407df322833d11412ed2b6
Sandesh <sandeshshingare2001@gmail.com>
2023-02-28 18:27:39 +0530

    done tuesday

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggle() {
+  var toggle_btn = document.getElementById("droplist");
+  //toggle_btn.style.display = "none";
+  //alert(toggle_btn.style.display);
+  if (toggle_btn.style.display === "none") {
+    toggle_btn.style.display = "block";
+  } else {
+    toggle_btn.style.display = "none";
+  }
+}
